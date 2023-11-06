b179a04ceeb0de64f582d8ecd2c5d6c97642cb29
Han Myat Thu <hanmyatthu111@gmail.com>
2023-11-06 21:46:07 +0700

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className='mt-20 ml-20'>
+        <h1>About</h1>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className='mt-20 ml-20'>
+        <h1>Home</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
